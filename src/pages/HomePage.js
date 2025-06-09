551a6411e167ad6c37644d0b1517c2d8bf2b6049
PrasannaLanka <prasu1760@gmail.com>
2025-06-09 23:59:34 +0530

    updated favicon and added settings button

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,14 +2,16 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./HomePage.css"; // Import the CSS file for styling
-
+import customButtonImage from "../assets/settings.png"; 
 function HomePage() {
     const navigate = useNavigate();
 
     const handleDifficultyClick = (difficulty) => {
         navigate(`/play?difficulty=${difficulty}`);
     };
-
+    const handleCustomClick = () => {
+        navigate("/custom");
+    };
     return (
         <div className="home-container">
             <h1>LET'S SOLVE SUDOKU</h1>
@@ -33,6 +35,12 @@ function HomePage() {
             >
                 Hard
             </button>
+            <img
+                src={customButtonImage}
+                alt="Custom Mode"
+                className="image-button"
+                onClick={handleCustomClick}
+            />
         </div>
     );
 }
